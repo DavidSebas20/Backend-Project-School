b0a12e2286a2a1179ac8de259d67aa20d6f9a917
DavidSebas20 <davidsebastian20@outlook.com>
2024-12-09 08:27:14 -0500

    add CORS

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from database import engine, Base
 from routers import events, locations, contacts
+from fastapi.middleware.cors import CORSMiddleware
 
 # Crear tablas en la base de datos
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Permite solicitudes desde cualquier origen (reemplaza con el dominio de tu frontend en producción)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes restringir esto a URLs específicas como ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos
+    allow_headers=["*"],  # Permite todas las cabeceras
+)
 
 # Incluir routers
 app.include_router(events.router, prefix="/api", tags=["events"])
